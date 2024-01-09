d369dc7b843716bfa25fbe2a59253088366ed7a4
rohanodel <rohanodel1997@gmail.com>
2024-01-09 22:51:10 +0530

    update

--- src/components/EducationPage.js
+++ src/components/EducationPage.js
@@ -1,51 +1,119 @@
+// import React from "react";
+// import "../styles/EducationPage.scss";
+
+// const EducationPage = () => {
+//   return (
+//     <div className="educationData">
+//       <div class="container">
+//         <div class="step completed">
+//           <div class="v-stepper">
+//             <div class="circle"></div>
+//             <div class="line"></div>
+//           </div>
+
+//           <div class="content">
+//             <div>SSC</div>
+//             <div>Maharashtra State Board</div>
+//             <div>Passout Year : March 2012</div> 
+//             {/* 55% */}
+//           </div>
+//         </div>
+
+//         <div class="step completed">
+//           <div class="v-stepper">
+//             <div class="circle"></div>
+//             <div class="line"></div>
+//           </div>
+
+//           <div class="content">
+//             <div>HSC</div>
+//             <div>Maharashtra State Board</div>
+//             <div>Passout Year : February 2014</div>
+//             {/* 48% */}
+//           </div>
+//         </div>
+
+//         <div class="step completed">
+//           <div class="v-stepper">
+//             <div class="circle"></div>
+//             <div class="line"></div>
+//           </div>
+
+//           <div class="content">
+//             <div>BCA (Bachelor of Computer Application)</div>
+//             <div>Savitribai Phule Pune University</div>
+//             <div>Passout Year : August 2021</div>
+//             {/* 76% */}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EducationPage;
+
+
+
+
+
 import React from "react";
 import "../styles/EducationPage.scss";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Typography from "@mui/material/Typography";
 
 const EducationPage = () => {
+  const steps = [
+    {
+      label: "SSC",
+      description: `Maharashtra State Board`,
+      newDescription: 'Passout Year : March 2012'
+    },
+    {
+      label: "HSC",
+      description: "Maharashtra State Board",
+      newDescription: 'Passout Year : February 2014'
+    },
+    {
+      label: "BCA (Bachelor of Computer Application)",
+      description: `Savitribai Phule Pune University`,
+      newDescription: 'Passout Year : August 2021'
+    },
+  ];
+
+  const CustomStepIcon = () => (
+    <div
+      style={{
+        width: 24,
+        height: 24,
+        borderRadius: "50%",
+        backgroundColor: "#44e4af",
+      }}
+    />
+  );
+
   return (
     <div className="educationData">
       <div class="container">
-        <div class="step completed">
-          <div class="v-stepper">
-            <div class="circle"></div>
-            <div class="line"></div>
-          </div>
-
-          <div class="content">
-            <div>SSC</div>
-            <div>Maharashtra State Board</div>
-            <div>Passout Year : March 2012</div> 
-            {/* 55% */}
-          </div>
-        </div>
-
-        <div class="step completed">
-          <div class="v-stepper">
-            <div class="circle"></div>
-            <div class="line"></div>
-          </div>
-
-          <div class="content">
-            <div>HSC</div>
-            <div>Maharashtra State Board</div>
-            <div>Passout Year : February 2014</div>
-            {/* 48% */}
-          </div>
-        </div>
-
-        <div class="step completed">
-          <div class="v-stepper">
-            <div class="circle"></div>
-            <div class="line"></div>
-          </div>
-
-          <div class="content">
-            <div>BCA (Bachelor of Computer Application)</div>
-            <div>Savitribai Phule Pune University</div>
-            <div>Passout Year : August 2021</div>
-            {/* 76% */}
-          </div>
-        </div>
+      <Box sx={{ maxWidth: 400 }}>
+          <Stepper orientation="vertical">
+            {steps.map((step) => (
+              <Step active={true} key={step.label}>
+                <StepLabel StepIconComponent={CustomStepIcon}>
+                  <div className="circleData">{step.label}</div>
+                </StepLabel>
+                <StepContent>
+                  <Typography>{step.description}</Typography>
+                  <Typography>{step.newDescription}</Typography>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
       </div>
     </div>
   );
--- src/components/ExperiencePage.js
+++ src/components/ExperiencePage.js
@@ -1,51 +1,120 @@
+// import React from "react";
+// import "../styles/ExperiencePage.scss";
+
+// const ExperiencePage = () => {
+//   return (
+//     <div className="experienceData">
+//       <div class="container">
+//         <div class="step completed">
+//           <div class="v-stepper">
+//             <div class="circle"></div>
+//             <div class="line"></div>
+//           </div>
+
+//           <div class="content">
+//             <div>Company Name : Aquis Tech</div>
+//             <div>January 2021 - December 2021</div>
+//           </div>
+//         </div>
+
+//         <div class="step completed">
+//           <div class="v-stepper">
+//             <div class="circle"></div>
+//             <div class="line"></div>
+//           </div>
+
+//           <div class="content">
+//             <div>Company Name : Polyglots</div>
+//             <div>January 2022 - August 2023</div>
+//           </div>
+//         </div>
+
+//         <div class="step completed">
+//           <div class="v-stepper">
+//             <div class="circle"></div>
+//             <div class="line"></div>
+//           </div>
+
+//           <div class="content">
+//             <div>Company Name = ....... </div>
+//             <div>present</div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="courseDetails">
+//         <div className="courseTitle">Course</div>
+//         <ul>
+//         <li className="courseData">Full Stack Software Developer</li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+// export default ExperiencePage;
+
+
+
+
 import React from "react";
 import "../styles/ExperiencePage.scss";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Typography from "@mui/material/Typography";
 
 const ExperiencePage = () => {
+  const steps = [
+    {
+      label: "Company Name : Aquis Tech",
+      description: `January 2021 - December 2021`,
+    },
+    {
+      label: "Company Name : Polyglots",
+      description: "January 2022 - August 2023",
+    },
+    {
+      label: "Company Name = ....... ",
+      description: `present....`,
+    },
+  ];
+
+  const CustomStepIcon = () => (
+    <div
+      style={{
+        width: 24,
+        height: 24,
+        borderRadius: "50%",
+        backgroundColor: "#44e4af",
+      }}
+    />
+  );
+
   return (
     <div className="experienceData">
       <div class="container">
-        <div class="step completed">
-          <div class="v-stepper">
-            <div class="circle"></div>
-            <div class="line"></div>
-          </div>
-
-          <div class="content">
-            <div>Company Name : Aquis Tech</div>
-            <div>January 2021 - December 2021</div>
-          </div>
-        </div>
-
-        <div class="step completed">
-          <div class="v-stepper">
-            <div class="circle"></div>
-            <div class="line"></div>
-          </div>
-
-          <div class="content">
-            <div>Company Name : Polyglots</div>
-            <div>January 2022 - August 2023</div>
-          </div>
-        </div>
-
-        <div class="step completed">
-          <div class="v-stepper">
-            <div class="circle"></div>
-            <div class="line"></div>
-          </div>
-
-          <div class="content">
-            <div>Company Name = ....... </div>
-            <div>present</div>
-          </div>
-        </div>
+        <Box sx={{ maxWidth: 400 }}>
+          <Stepper orientation="vertical">
+            {steps.map((step) => (
+              <Step active={true} key={step.label}>
+                <StepLabel StepIconComponent={CustomStepIcon}>
+                  <div className="circleData">{step.label}</div>
+                </StepLabel>
+                <StepContent>
+                  <Typography>{step.description}</Typography>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
       </div>
 
       <div className="courseDetails">
         <div className="courseTitle">Course</div>
         <ul>
-        <li className="courseData">Full Stack Software Development</li>
+          <li className="courseData">Full Stack Software Developer</li>
         </ul>
       </div>
     </div>
